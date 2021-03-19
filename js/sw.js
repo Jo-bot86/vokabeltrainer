79e3ff682987169360302e0915bf563f889a0f1d
Josef Weldemariam <j.weldemariam@web.de>
2021-03-19 22:39:40 +0100

    test

--- js/sw.js
+++ js/sw.js
@@ -7,21 +7,21 @@ var filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-	e.respondWith(
-		caches.match(e.request).then(function(response) {
-		// return the cached content if available
-		// if not, fetch the request 
-		return response || fetch(e.request);
-		})
-	);
+/* Serve cached content when offline Test*/
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      // return the cached content if available
+      // if not, fetch the request 
+      return response || fetch(e.request);
+    })
+  );
 });
